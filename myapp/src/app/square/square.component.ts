6f8351da7b0a9571c39d002800216080200df87e
mattgoldsmith <goldsmithmj91@gmail.com>
2023-06-25 16:01:23 +0700

    Finalise the app and deploy to firebase

--- myapp/src/app/square/square.component.ts
+++ myapp/src/app/square/square.component.ts
@@ -3,20 +3,24 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button>{{ value }}</button>
+    <button *ngIf="!value">{{ value }}</button>
+    <button class="o" *ngIf="value == 'O'">{{ value }}</button>
+    <button class="x" *ngIf="value == 'X'">{{ value }}</button>
   `,
   styles: [
     `button {
         height: 100%; 
         width: 100%;
         font-size: 4em;
-        background: MediumAquaMarine;
+        background: linear-gradient(#937ad2, #7e64c0);
+        color: white;
+        font-weight: bold;
     }`,
-    `app-square[ng-reflect-value=X] button {
-        background: MediumSeaGreen;
+    `button.x {
+        background: linear-gradient(to right, #43d1a9, #0dc091);
     }`,
-    `app-square[ng-reflect-value=O] button {
-        background: MediumPurple;
+    `button.o {
+        background: linear-gradient(to right, #36a7fa, #1688e5);;
     }`
   ]
 })
